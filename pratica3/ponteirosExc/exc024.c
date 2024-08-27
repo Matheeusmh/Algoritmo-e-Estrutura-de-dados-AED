f585e3fcda7b32b1aa5d173978167ec5e798ac08
Matheus Henrique <matheushenrique.mh080@gmail.com>
2024-08-27 16:07:08 -0300

    mod

--- pratica3/ponteirosExc/exc024.c
+++
@@ -1,9 +0,0 @@
-/* Escreva uma func¸ao que receba um array de inteiros V e os enderec¸os de duas vari ˜ aveis ´
-inteiras, min e max, e armazene nessas variaveis o valor m ´ ´ınimo e maximo do array. ´
-Escreva tambem uma func¸ ´ ao main que use essa func¸ ˜ ao.*/
-#include <stdio.h>
-#include <stdlib.h>
-
-int main(void) {
-    
-}
