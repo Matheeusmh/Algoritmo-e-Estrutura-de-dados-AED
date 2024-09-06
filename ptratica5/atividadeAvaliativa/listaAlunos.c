662dfb67bb8cb96774184e8e1ff2005a370aa8f8
Matheus Henrique <matheushenrique.mh080@gmail.com>
2024-09-05 23:15:37 -0300

    fix

--- ptratica5/atividadeAvaliativa/listaAlunos.c
+++ ptratica5/atividadeAvaliativa/listaAlunos.c
@@ -1,26 +1,186 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 #include "listaAlunos.h"
-#include<stdlib.h>
-#include<stdio.h>
-#include<string.h>
 
-Aluno* criarListaVazia() {
+Aluno *cabecaMedia = NULL;
+Aluno *cabeca = NULL;
+
+Aluno *criarListaVazia()
+{
     return NULL;
 }
 
-Aluno* inserirAluno(char nome[], float notaPratica, float notaTeorica, Aluno *lAluno) { 
-    Aluno* novo;
-
+Aluno *inserirAluno(char nome[], float notaPratica, float notaTeorica)
+{
+    Aluno *novo;
     novo = (Aluno *)malloc(sizeof(Aluno));
+    float media;
 
-    if(novo == NULL) {
+    if (novo == NULL)
+    {
         printf("\nNAO foi possivel alocar memoria!!\n");
-        return NULL;
+        return cabeca;
     }
 
     strcpy(novo->nome, nome);
     novo->notaPratica = notaPratica;
     novo->notaTeorica = notaTeorica;
-    novo->prox = lAluno;
+
+    media = (notaPratica + notaTeorica) / 2;
+
+    if (media > 70)
+    {
+        Aluno *mediaMaior;
+        mediaMaior = (Aluno *)malloc(sizeof(Aluno));
+
+        if (mediaMaior == NULL)
+        {
+            printf("\nNAO foi possivel alocar memoria!!\n");
+            return cabeca;
+        }
+        else
+        {
+            mediaMaior->notaPratica = notaPratica;
+            mediaMaior->notaTeorica = notaTeorica;
+            strcpy(mediaMaior->nome, nome);
+            mediaMaior->prox = cabecaMedia;
+            cabecaMedia = mediaMaior;
+        }
+    }
+
+    novo->prox = cabeca;
+    cabeca = novo;
 
     return novo;
+}
+
+Aluno *encontrarNotas(char *nome)
+{
+    Aluno *ptr;
+    ptr = cabeca;
+
+    while (ptr != NULL)
+    {
+        if (strcmp(ptr->nome, nome) == 0)
+        {
+            printf("Nota pratica: %.2f\n", ptr->notaPratica);
+            printf("Nota teorica: %.2f\n", ptr->notaTeorica);
+            return ptr;
+        }
+        ptr = ptr->prox;
+    }
+
+    printf("Aluno nao encontrado!!\n");
+
+    return ptr;
+}
+
+Aluno *mediaSuperior()
+{
+    Aluno *media;
+    media = cabecaMedia;
+
+    while (media != NULL)
+    {
+        printf("Nome: %s\n", media->nome);
+        printf("Nota pratica: %.2f\n", media->notaPratica);
+        printf("Nota teorica: %.2f\n\n", media->notaTeorica);
+
+        media = media->prox;
+    }
+
+    return media;
+}
+
+Aluno *removerAluno(char *nome)
+{
+    Aluno *ptr, *ant, *ptrMedia, *antMedia;
+    ptr = cabeca;
+    ant = cabeca;
+    ptrMedia = cabecaMedia;
+    antMedia = cabecaMedia;
+
+    while (ptrMedia != NULL)
+    {
+        if (strcmp(ptrMedia->nome, nome) == 0)
+        {
+            antMedia->prox = ptrMedia->prox;
+            if (ptrMedia == cabecaMedia)
+                cabecaMedia = cabecaMedia->prox;
+            break;
+        }
+
+        antMedia = ptrMedia;
+        ptrMedia = ptrMedia->prox;
+    }
+
+    while (ptr != NULL)
+    {
+        if (strcmp(ptr->nome, nome) == 0)
+        {
+            ant->prox = ptr->prox;
+            if (ptr == cabeca)
+                cabeca = cabeca->prox;
+            printf("Aluno removido com sucesso!!\n");
+            return ant;
+        }
+
+        ant = ptr;
+        ptr = ptr->prox;
+    }
+
+    printf("Aluno nao encontrado!!\n");
+
+    return ptr;
+}
+
+void menuAluno()
+{
+    char nome[50];
+    int op = 10;
+    float notaPratica, notaTeorica;
+
+    while (op != 0)
+    {
+        printf("\n[1] Inserir um aluno\n");
+        printf("[2] Encontrar notas de um aluno\n");
+        printf("[3] Lista de alunos com media superior a 70\n");
+        printf("[4] Remover um aluno\n");
+        printf("[0] Sair do programa\n Escolha: ");
+        scanf("%d", &op);
+        getchar();
+
+        switch (op)
+        {
+        case 1:
+            printf("\nDigite o nome do aluno: ");
+            scanf(" %[^\n]", nome);
+            printf("Digite a nota pratica do aluno: ");
+            scanf("%f", &notaPratica);
+            printf("Digite a nota teorica do aluno: ");
+            scanf("%f", &notaTeorica);
+            inserirAluno(nome, notaPratica, notaTeorica);
+            break;
+        case 2:
+            printf("\nDigite o nome do aluno que deseja encontrar: ");
+            scanf(" %[^\n]", nome);
+            encontrarNotas(nome);
+            break;
+        case 3:
+            printf("\n");
+            mediaSuperior();
+            break;
+        case 4:
+            printf("\nDigite o aluno a ser removido: ");
+            scanf("  %[^\n]", nome);
+            removerAluno(nome);
+            break;
+        case 0:
+            break;
+        default:
+            printf("OPCAO INVALIDA!!");
+            break;
+        }
+    }
 }
--- ptratica5/atividadeAvaliativa/listaAlunos.h
+++ ptratica5/atividadeAvaliativa/listaAlunos.h
@@ -1,14 +1,23 @@
 #ifndef LISTA_ALUNOS_H
 #define LISTA_ALUNOS_H
 
-typedef struct aluno{
+typedef struct aluno
+{
     char nome[50];
     float notaPratica;
     float notaTeorica;
     struct aluno *prox;
 } Aluno;
 
-Aluno* inserirAluno(char nome[], float notaPratica, float notaTeorica, Aluno *lAluno);
-Aluno* criarListaVazia();
+extern Aluno *cabeca;
+extern Aluno *cabecaMedia;
+
+Aluno *criarListaVazia();
+Aluno *inserirAluno(char nome[], float notaPratica, float notaTeorica);
+Aluno *encontrarNotas(char *nome);
+Aluno *mediaSuperior();
+Aluno *removerAluno(char *nome);
+void menuAluno();
+
 
 #endif
