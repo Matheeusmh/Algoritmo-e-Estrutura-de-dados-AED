23ba57b3ecf79714eb44eb6153cf70a7b2a76034
Matheus Henrique <matheushenrique.mh080@gmail.com>
2024-10-20 08:29:55 -0300

    add

--- pratica10/main.c
+++ pratica10/main.c
@@ -1,29 +1,64 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+typedef struct {
+    char nome[50];
+    int posicao;
+} Pessoa;
 
 typedef struct no{
-    int posicao;
+    Pessoa pessoa; 
     struct no *prox;
     struct no *ant;
 } No;
 
-void iniciarLista(No **circulo) {
+typedef No* Novo;
+
+void iniciarLista(No **circulo, char *nome) {
     (*circulo) = (No *) malloc(sizeof(No));
 
-    (*circulo)->posicao = 1;
+    strcpy((*circulo)->pessoa.nome, nome);
+    (*circulo)->pessoa.posicao = 1;
     (*circulo)->ant = *circulo;
     (*circulo)->prox = *circulo;
 
 }
 
-void inserirInicio(No **circulo) {
+void inserirInicio(No **circulo, char *nome) {
     if(*circulo == NULL) {
-        iniciarLista(circulo);
+        iniciarLista(circulo, nome);
         return;
     }
+
+    Novo novo;
+    novo = (No *)malloc(sizeof(No));
+
+    strcpy(novo->pessoa.nome, nome);
+    novo->pessoa.posicao = 0;
+    
+    Novo aux = *circulo;
+
+    do {
+        aux = aux->prox;
+    } while(aux->prox != *circulo);
+
+    aux->prox = novo;
+    novo->ant = aux;
+    novo->prox = *circulo;
+
+    aux = *circulo;
+
+    do {
+        aux->pessoa.posicao++;
+        aux = aux->prox;
+    } while(aux != *circulo);
+    
 }
 
 void menu(No **circulo) {
     int op;
+    char nome[50];
 
     while(1) {
         printf("[1] Inserir nó no início\n");
@@ -37,7 +72,7 @@ void menu(No **circulo) {
 
         switch(op) {
             case 1:
-                inserirInicio(circulo);
+                inserirInicio(circulo, nome);
                 break;
         }
     }
